5a738c1a6a1fc4d3da9e0c00b929c2b56e7f94a8
heedahan16 <devhee.handa@gmail.com>
2023-11-16 16:07:39 +0900

    파이썬 자동화

---
+++ Automation/test.bat
@@ -0,0 +1,1 @@
+"C:/Anaconda3/envs/pybook/python.exe" "C:/Users/Desktop/sessac/special lecture/Automation/test.py"
---
+++ Automation/test.py
@@ -0,0 +1,2 @@
+with open('C:/Users/Desktop/sessac/special lecture/Automation/test.json', 'w'):
+    pass
